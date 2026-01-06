cef87578f1b56322429444f19649c25e4de27f05
Victoria Vignon <vignonvictoria@gmail.com>
2026-01-06 14:50:43 +0100

    update readme

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>ToutCru</h1>
+  <p>
+    ToutCru, c'est l'app qui vous accompagne pour l'alimentation de votre compagnon !
+  </p>
+</template>
+
+<style scoped></style>
