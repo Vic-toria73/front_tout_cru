c67bd01fbd2e197e76bac9a2534863ec65e80a44
Victoria Vignon <vignonvictoria@gmail.com>
2026-01-18 20:51:36 +0100

    WIP, add packfile composables for useAuth, update navbar slidebar guest or user

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,40 +1,15 @@
-<template>
-  <div class="bg-primary p-4 flex justify-between items-center">
-    <div class="text">ToutCru</div>
-    <div class="flex gap-4">
-      <router-link to="/home" class="text text-2xl">
-        <PawPrint />
-      </router-link>
-      <button @click="toggleMenu" class="text text-2xl">
-        <Menu />
-      </button>
-
-      <Transition name="slide">
-      <div v-if="isMenuOpen" class="fixed top-0 right-0 h-full w-64 bg-primary shadow-lg z-50 p-6 
-         flex flex-col justify-center gap-4">
-        <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
-         @click="isMenuOpen = false" to="/me">Mon compte</router-link>
-        <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
-        @click="isMenuOpen = false" to="/mypets">Mes animaux</router-link>
-        <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
-        @click="isMenuOpen = false" to="/calculator">Calculer</router-link>
-        <button @click="onLogout" class="h-12 px-4 hover:bg-[#F90404]/100 rounded-xl transition">Déconnexion</button>
-      </div>
-      </Transition>
-
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { logout } from '../services/authService';
 import { Menu, PawPrint } from 'lucide-vue-next';
 import { useRouter } from 'vue-router';
 import { ref } from 'vue';
+import { useAuth } from '../composables/useAuth';
 
 defineOptions({
   name: 'AppNavbar',
 });
+
+const { isAuthenticated } = useAuth();
 
 const isMenuOpen = ref(false);
 
@@ -50,6 +25,44 @@ function onLogout() {
   router.push('/home');
 }
 </script>
+
+<template>
+  <div class="bg-primary p-4 flex justify-between items-center">
+    <div class="text">ToutCru</div>
+    <div class="flex gap-4">
+      <router-link to="/home" class="text text-2xl">
+        <PawPrint />
+      </router-link>
+      <button @click="toggleMenu" class="text text-2xl">
+        <Menu />
+      </button>
+
+      <Transition name="slide">
+        
+          <div v-if="isMenuOpen" class="fixed top-0 right-0 h-full w-64 bg-primary shadow-lg z-50 p-6 
+         flex flex-col justify-center gap-4">
+         <div v-if="isAuthenticated">
+            <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
+              @click="isMenuOpen = false" to="/me">Mon compte</router-link>
+            <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
+              @click="isMenuOpen = false" to="/mypets">Mes animaux</router-link>
+            <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition"
+              @click="isMenuOpen = false" to="/calculator">Calculer</router-link>
+            <button @click="onLogout"
+              class="h-12 px-4 hover:bg-[#F90404]/100 rounded-xl transition">Déconnexion</button>
+          </div>
+          
+          <div v-else class="fixed top-0 right-0 h-full w-64 bg-primary shadow-lg z-50 p-6 
+         flex flex-col justify-center gap-4">
+            <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition" to="/login">Connexion</router-link>
+            <router-link class="flex items-center h-12 px-4 hover:bg-background-alt/100 rounded-xl transition" to="/register">Inscription</router-link>
+          </div>
+        </div>
+      </Transition>
+
+    </div>
+  </div>
+</template>
 
 <style scoped>
 .slide-enter-active,
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,34 +1,35 @@
-import api from './api'
+import api from './api';
+import { useAuth } from '../composables/useAuth';
 
 export interface RegisterData {
   email: string
   firstName: string
   password: string
   confirmPassword: string
-}
+};
 
 export const register = async (data: RegisterData) => {
   const response = await api.post('/users', data)
   return response.data
-}
+};
 
 export interface LoginData {
   email: string
   password: string
-}
+};
 
 export const login = async (data: LoginData) => {
   const response = await api.post('/auth/login', data)
-
-  if (response.status !== 200) throw new Error(`Erreur HTTP ${response.status}`);
+  const { login: setAuth } = useAuth();
 
   if (response.data.token) {
-    localStorage.setItem('token', response.data.token);
+    setAuth(response.data.token);
   }
-  console.log(response);
+
   return response.data;
 };
 
 export const logout = () => {
-  localStorage.removeItem("token");
+  const { logout: clearAuth } = useAuth();
+  clearAuth();
 };
